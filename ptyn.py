52a088f0bcc637da51d440701b6566e1fc2ecb4c
muhwezititus <muhwezititus13@gmail.com>
2019-06-29 13:25:23 +0300

    the very first commit

---
+++ ptyn.py
@@ -0,0 +1,7 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print "Sue keep qiet"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("Sue keep qiet")?
+>>> print "keep qiet"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("keep qiet")?
+>>> print "gud to go"
